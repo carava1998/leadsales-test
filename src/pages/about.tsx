66c54565c2d0b8bd0be6ea8123f9ce9ddb9e58b7
Alejandro Ochoa <alex_8a_garcia@hotmail.com>
2022-06-06 22:58:31 -0500

    Full implementation

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
 import NavBar from "../components/NavBar";
 
-const Home: NextPage = () => {
+const About: NextPage = () => {
   return (
     <>
       <NavBar />
       <h2 className="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">
-        Home
+        About
       </h2>
     </>
   );
 };
 
-export default Home;
+export default About;
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
 import NavBar from "../components/NavBar";
 
-const Home: NextPage = () => {
+const Menu: NextPage = () => {
   return (
     <>
       <NavBar />
       <h2 className="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">
-        Home
+        Menu
       </h2>
     </>
   );
 };
 
-export default Home;
+export default Menu;
