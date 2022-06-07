80e8c2fdca197659c84dd89576f7d56da265aab1
alegleason <alejandrogleasonm@gmail.com>
2022-06-06 20:14:04 -0500

    Make div clickable and not just text

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,10 +1,28 @@
+import Link from 'next/link'
+
 const NavBar = () => {
   return (
     <div className="flex justify-center flex-row pt-3 ">
-      <div className="border-l border-t border-b px-2">Home</div>
-      <div className="border-l border-t border-b px-2">About</div>
-      <div className="border-l border-t border-b px-2">Contact</div>
-      <div className="border px-2">Menu</div>
+      <Link href="/">
+        <div className="border-l border-t border-b px-2 cursor-pointer">
+            Home
+        </div>
+      </Link>
+      <Link href="/about">
+        <div className="border-l border-t border-b px-2 cursor-pointer">
+          About
+        </div>
+      </Link>
+      <Link href="/contact">
+        <div className="border-l border-t border-b px-2 cursor-pointer">
+          Contact
+        </div>
+      </Link>
+      <Link href="/menu">
+        <div className="border px-2 cursor-pointer">
+          Menu
+        </div>
+      </Link>
     </div>
   )
 }
